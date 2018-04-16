1b2610b07f995bdac22fcd34d12d3a48fcb7d2f8
Eyuel <eyuel.taddese4@gmail.com>
2018-04-15 22:11:56 -0400

    Fixing bug passwordEncoder

--- src/main/java/com/fenast/ibextube/config/oauth2/ServerSecurityConfig.java
+++ src/main/java/com/fenast/ibextube/config/oauth2/ServerSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 /*@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)*/
